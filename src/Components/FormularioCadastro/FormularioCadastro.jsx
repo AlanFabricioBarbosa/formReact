c200524ddd788671e69fbcd876c509d80e2883bd
Alan Fabrício Barbosa da Silva <alanfabriciobarbosa1@gmail.com>
2023-08-31 21:28:06 -0300

    :sparkles: feat: Iniciando a validação do formulário

--- src/Components/FormularioCadastro/FormularioCadastro.jsx
+++ src/Components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,15 +2,58 @@ import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
 import Switch from '@mui/material/Switch'
 import FormControlLabel from '@mui/material/FormControlLabel'
-import React from 'react'
+import React, { useState } from 'react'
 
 
 function FormularioCadastro() {
+    const [nome, setNome] = useState("")
+    const [sobrenome, setSobrenome] = useState("")
+    const [cpf, setCpf] = useState("")
+
     return(
-        <form>
-            <TextField id='nome' label='Nome:' variant='outlined' fullWidth  margin='normal'/>
-            <TextField id='sobrenome' label='Sobrenome:' variant='outlined' fullWidth  margin='normal'/>
-            <TextField id='cpf' label='CPF:' variant='outlined' fullWidth  margin='normal'/>
+        <form onSubmit={(event)=> {
+                event.preventDefault()
+                console.log(nome, sobrenome, cpf)
+            }
+        }>
+            <TextField
+                value={nome}
+                onChange={(event) => {
+                        let tmpNome = event.target.value
+                        if(nome.length >= 3){
+                            tmpNome = tmpNome.substring(0, 3)
+                        }
+                    
+                        setNome(tmpNome)
+                    }
+                }
+                id='nome' 
+                label='Nome:' 
+                variant='outlined' 
+                fullWidth  
+                margin='normal'/>
+            <TextField 
+                value={sobrenome}
+                onChange={(event) => {
+                        setSobrenome(event.target.value)
+                    }
+                }
+                id='sobrenome' 
+                label='Sobrenome:' 
+                variant='outlined' 
+                fullWidth  
+                margin='normal'/>
+            <TextField
+                value={cpf}
+                onChange={(event) => {
+                        setCpf(event.target.value)
+                    }
+                }
+                id='cpf' 
+                label='CPF:' 
+                variant='outlined' 
+                fullWidth  
+                margin='normal'/>
             <FormControlLabel 
                 label='Promoções' 
                 control={<Switch name='promocoes' defaultChecked />} 
